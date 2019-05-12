60d93e112daa7f74e51d782c755c19aab5312dbb
Jan Buschtöns <jan@buschtoens.me>
2019-05-12 12:35:42 +0200

    test: add basic coverage for document and window

--- tests/integration/helpers/on-window-test.js
+++ tests/integration/helpers/on-window-test.js
@@ -1,17 +1,38 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, click, resetOnerror } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Helper | on-window', function(hooks) {
   setupRenderingTest(hooks);
+  hooks.afterEach(() => resetOnerror());
 
-  // Replace this with your real tests.
-  test('it renders', async function(assert) {
-    this.set('inputValue', '1234');
+  test('it basically works', async function(assert) {
+    assert.expect(6);
 
-    await render(hbs`{{on-window inputValue}}`);
+    this.someMethod = function(event) {
+      assert.ok(this instanceof Window, 'this context is the window');
+      assert.ok(
+        event instanceof MouseEvent,
+        'first argument is a `MouseEvent`'
+      );
+      assert.strictEqual(
+        event.target.tagName,
+        'BUTTON',
+        'correct element tagName'
+      );
+      assert.dom(event.target).hasAttribute('data-foo', 'test-element');
+    };
 
-    assert.equal(this.element.textContent.trim(), '1234');
+    await render(hbs`
+      {{on-window "click" this.someMethod}}
+      <button type="button" data-foo="test-element"></button>
+    `);
+
+    assert.counts({ adds: 1, removes: 0 });
+
+    await click('button');
+
+    assert.counts({ adds: 1, removes: 0 });
   });
 });
